ee8d148b696c7f496b27d577b41bc8a3f12fcfbe
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-01-21 13:35:22 -0300

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Preview from "./pages/Preview"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Preview />
+  </React.StrictMode>
+)
