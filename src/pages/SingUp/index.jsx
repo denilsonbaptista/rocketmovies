b6185ef7dfa1d00f779bd96383bf4d09d61926ac
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-02-24 17:26:30 -0300

    logic of registration user in the database

--- src/pages/SingUp/index.jsx
+++ src/pages/SingUp/index.jsx
@@ -1,26 +1,72 @@
-import { Link } from "react-router-dom"
-import { BsArrowLeft } from "react-icons/bs"
-import { FiMail, FiLock, FiUser } from "react-icons/fi"
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { BsArrowLeft } from 'react-icons/bs'
+import { FiMail, FiLock, FiUser } from 'react-icons/fi'
 
-import { Input } from "../../components/Input"
-import { Button } from "../../components/Button"
+import { api } from '../../services/api'
 
-import { Container, Form, Background } from "./styles"
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+
+import { Container, Form, Background } from './styles'
 
 export function SingUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleSingUp(event) {
+    event.preventDefault()
+
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado')
+        navigate(-1)
+      })
+      .catch(error => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Não foi possível cadastrar')
+        }
+      })
+  }
+
   return (
     <Container>
-      <Form>
+      <Form onSubmit={handleSingUp}>
         <h1>RocketMovies</h1>
         <p>Aplicação para acompanhar tudo que assistir</p>
 
         <h2>Crei sua conta</h2>
 
-        <Input placeholder="Nome" type="text" icon={FiUser} />
-        <Input placeholder="E-mail" type="E-mail" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="email"
+          icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" type="submit" />
 
         <Link to="/">
           <BsArrowLeft />
