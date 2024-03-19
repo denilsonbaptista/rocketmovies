819d884a64759b2adb246f90630e138e5d94fbf1
denilsonbaptista <baptistadenilsonbp@gmail.com>
2024-03-18 22:10:14 -0300

    updated link of api

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'https://rocketmovies-api-p1n5.onrender.com',
   withCredentials: true,
 })
